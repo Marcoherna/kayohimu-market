7cf78d6448168fc71934c5f105fdb35aae9ceb88
marco <marcotaku@gmail.com>
2025-03-24 09:58:56 -0300

    primer commit

---
+++ src/main/java/com/kayohimu/market/KayohimuMarketApplication.java
@@ -0,0 +1,13 @@
+package com.kayohimu.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KayohimuMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KayohimuMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kayohimu/market/KayohimuMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kayohimu.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KayohimuMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
